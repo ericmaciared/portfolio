489f9f2f443ab7e5a1133fadfc16f2f79c23ee1d
Eric Macià <ericmaciared@gmail.com>
2023-05-23 13:15:33 +0200

    add koi background

---
+++ js/koi.js
@@ -0,0 +1,137 @@
+class Koi extends Flagellum {
+
+  constructor(skinImg) {
+    super(skinImg);
+
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+    this.maxSpeed = random(0.8, 1.9);
+    this.rushSpeed = random(3, 6);
+    this.maxForce = 0.2;
+    this.wandertheta = random(0, 360);
+
+    this.timeCheck = false;
+    this.timeCount = 0;
+    this.lastTimeCheck = 0;
+    this.timeCountLimit = 10;
+  }
+
+  // Main live loop
+  // TODO: add a "brain" to the fish
+  live() {
+    //if (mouseIsPressed) {
+      let mouseLocation = createVector(mouseX - width/2, mouseY - height/2);
+      if (this.location.dist(mouseLocation) < 100) {
+        this.evade(mouseLocation);
+      }
+    //} 
+    this.wander();
+    this.update();
+    this.borders();
+    this.display();
+  }
+
+  steer(target, slowdown) {
+    let steer;
+    let desired = target.sub(this.location);
+    let d = desired.mag();
+
+    if (d > 0) {
+      desired.normalize();
+
+      if (slowdown && d < 100) {
+        desired.mult(this.maxSpeed * (d / 100));
+      }
+      else {
+        desired.mult(this.maxSpeed);
+      }
+
+      steer = desired.sub(this.velocity);
+      steer.limit(this.maxForce);
+    }
+    else {
+      steer = createVector(0, 0);
+    }
+
+    return steer;
+  }
+
+  seek(target) {
+    this.acceleration.add(this.steer(target, false));
+  }
+
+  flee(target) {
+    this.acceleration.sub(this.steer(target, false));
+  }
+  
+  evade(target) {
+    this.timeCheck = true;
+    if (dist(target.x, target.y, this.location.x, this.location.y) < 100) {
+      let lookAhead = this.location.dist(target) / (this.maxSpeed * 2);
+      let predictedTarget = createVector(target.x - lookAhead, target.y - lookAhead);
+      this.flee(predictedTarget);
+    }
+  }
+
+  wander() {
+    let wanderR = 10;
+    let wanderD = 100;
+    let change = 0.05;
+
+    this.wandertheta += random(-change, change);
+
+    let circleLocation = this.velocity.copy();
+    circleLocation.normalize();
+    circleLocation.mult(wanderD);
+    circleLocation.add(this.location);
+
+    let circleOffset = createVector(wanderR * cos(this.wandertheta), wanderR * sin(this.wandertheta));
+    let target = circleLocation.add(circleOffset);
+    this.acceleration.add(this.seek(target));
+  }
+
+  update() {
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.location.add(this.velocity);
+    this.acceleration.mult(0);
+    super.muscleFreq = norm(this.velocity.mag(), 0, 1) * 0.06;
+    super.move();
+
+    // Rotate fish
+    let angle = this.velocity.heading() + radians(180);
+    this.theta = degrees(angle) + 180;
+
+    if (this.timeCheck) {
+      if (millis() > this.lastTimeCheck + 200) {
+        this.lastTimeCheck = millis();
+
+        if (this.timeCount <= this.timeCountLimit) {
+          // derease maxSpeed in relation with time cicles
+          // this formula needs a proper look
+          this.maxSpeed = this.rushSpeed - (norm(this.timeCount, 0, this.timeCountLimit) * 3);
+          this.timeCount++;
+        }
+        else if (this.timeCount >= this.timeCountLimit) {
+          // once the time cicle is complete
+          // resets timer variables,
+          this.timeCount = 0;
+          this.timeCheck = false;
+
+          // set default speed values
+          this.maxSpeed = random(0.8, 1.9);
+          this.maxForce = 0.2;
+        }
+      }
+    }
+  }
+
+  // wrapper, appear opposit side
+  borders() {
+    if (this.location.x < -windowWidth/2 - this.skin.width) this.location.x = windowWidth/2;
+    if (this.location.x > windowWidth/2 + this.skin.width) this.location.x = -windowWidth/2;
+    if (this.location.y < -windowHeight/2 -this.skin.width) this.location.y = windowHeight / 2;
+    if (this.location.y > windowHeight/2 + this.skin.width) this.location.y = -windowHeight / 2;
+  }
+}
+  
--- js/pond.js
+++ js/pond.js
@@ -1,17 +1,43 @@
+
+let numKois;
+let kois  = [];
+let koiSkins = [];
 let canvas;
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(windowWidth, windowHeight, true);
+    let newKois = round(windowWidth / 200);
+    if (numKois < newKois) {
+        for (let i = 0; i < newKois; i++) {
+            let skinId = round(random(0, 9));
+            kois.push(new Koi(koiSkins[skinId]));
+        }
+    }
+    numKois = round(windowWidth / 200);
 }
 
-function setup(){
-    canvas = createCanvas(windowWidth, windowHeight);
+function preload() {
+    for (let i = 0; i < 10; i++) koiSkins[i] = "img/skin-" + i + ".png";
+}
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight, WEBGL);
     canvas.position(0, 0);
     canvas.style("z-index: -100");
+    smooth();
+    pixelDensity(2);
+
+    numKois = round(windowWidth / 200);
+
+    for (let i = 0; i < numKois; i++) {
+        let skinId = round(random(0, 9));
+        kois[i] = new Koi(koiSkins[skinId]);
+    }
 }
 
-function draw(){
+function draw() {
+    // TODO: Dynamic water? Noise background for coloring?
     background(238);
-    noStroke();
-    ellipse(mouseX, mouseY, 200, 200);
+
+    for (let i = 0; i < numKois; i++) kois[i].live();
 }
